92d4832a469f7f514a9c32f7ab826c8f0f5ed4c8
Robin <robinkqin@163.com>
2024-05-19 23:40:15 +0800

    add custom.el

---
+++ custom-post.el
@@ -0,0 +1,11 @@
+;;; custom-post.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
+;;; Commentary:
+;;;       Put your own configurations in custom-post.el to override default configurations.
+;;; Code:
+
+(message "custom-post.el loading...")
+
+
+(message "custom-post.el loaded")
+
+;;; custom-post.el ends here
